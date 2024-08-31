f31cc20a2d67622a3a88926c0a6f3df4e85bdfe6
BolleA7X <alessio.bollea@live.it>
2024-08-31 20:55:14 +0200

    Implemented 'personal decisions' tables

---
+++ src/model/tables/decisions.ts
@@ -0,0 +1,306 @@
+import Dice from '../dice'
+import { Table } from '../table'
+
+/* BACKGROUND */
+
+export const acolyte = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@acolyte-0", modifier: 0, actions: []},
+        {limit: 2, text: "@acolyte-1", modifier: 0, actions: []},
+        {limit: 3, text: "@acolyte-2", modifier: 0, actions: []},
+        {limit: 4, text: "@acolyte-3", modifier: 0, actions: []},
+        {limit: 5, text: "@acolyte-4", modifier: 0, actions: []},
+        {limit: 6, text: "@acolyte-5", modifier: 0, actions: []},
+    ]
+)
+
+export const charlatan = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@charlatan-0", modifier: 0, actions: []},
+        {limit: 2, text: "@charlatan-1", modifier: 0, actions: []},
+        {limit: 3, text: "@charlatan-2", modifier: 0, actions: []},
+        {limit: 4, text: "@charlatan-3", modifier: 0, actions: []},
+        {limit: 5, text: "@charlatan-4", modifier: 0, actions: []},
+        {limit: 6, text: "@charlatan-5", modifier: 0, actions: []},
+    ]
+)
+
+export const criminal = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@criminal-0", modifier: 0, actions: []},
+        {limit: 2, text: "@criminal-1", modifier: 0, actions: []},
+        {limit: 3, text: "@criminal-2", modifier: 0, actions: []},
+        {limit: 4, text: "@criminal-3", modifier: 0, actions: []},
+        {limit: 5, text: "@criminal-4", modifier: 0, actions: []},
+        {limit: 6, text: "@criminal-5", modifier: 0, actions: []},
+    ]
+)
+
+export const entertainer = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@entertainer-0", modifier: 0, actions: []},
+        {limit: 2, text: "@entertainer-1", modifier: 0, actions: []},
+        {limit: 3, text: "@entertainer-2", modifier: 0, actions: []},
+        {limit: 4, text: "@entertainer-3", modifier: 0, actions: []},
+        {limit: 5, text: "@entertainer-4", modifier: 0, actions: []},
+        {limit: 6, text: "@entertainer-5", modifier: 0, actions: []},
+    ]
+)
+
+export const folkHero = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@folkhero-0", modifier: 0, actions: []},
+        {limit: 2, text: "@folkhero-1", modifier: 0, actions: []},
+        {limit: 3, text: "@folkhero-2", modifier: 0, actions: []},
+        {limit: 4, text: "@folkhero-3", modifier: 0, actions: []},
+        {limit: 5, text: "@folkhero-4", modifier: 0, actions: []},
+        {limit: 6, text: "@folkhero-5", modifier: 0, actions: []},
+    ]
+)
+
+export const guildArtisan = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@guildartisan-0", modifier: 0, actions: []},
+        {limit: 2, text: "@guildartisan-1", modifier: 0, actions: []},
+        {limit: 3, text: "@guildartisan-2", modifier: 0, actions: []},
+        {limit: 4, text: "@guildartisan-3", modifier: 0, actions: []},
+        {limit: 5, text: "@guildartisan-4", modifier: 0, actions: []},
+        {limit: 6, text: "@guildartisan-5", modifier: 0, actions: []},
+    ]
+)
+
+export const hermit = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@hermit-0", modifier: 0, actions: []},
+        {limit: 2, text: "@hermit-1", modifier: 0, actions: []},
+        {limit: 3, text: "@hermit-2", modifier: 0, actions: []},
+        {limit: 4, text: "@hermit-3", modifier: 0, actions: []},
+        {limit: 5, text: "@hermit-4", modifier: 0, actions: []},
+        {limit: 6, text: "@hermit-5", modifier: 0, actions: []},
+    ]
+)
+
+export const noble = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@noble-0", modifier: 0, actions: []},
+        {limit: 2, text: "@noble-1", modifier: 0, actions: []},
+        {limit: 3, text: "@noble-2", modifier: 0, actions: []},
+        {limit: 4, text: "@noble-3", modifier: 0, actions: []},
+        {limit: 5, text: "@noble-4", modifier: 0, actions: []},
+        {limit: 6, text: "@noble-5", modifier: 0, actions: []},
+    ]
+)
+
+export const outlander = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@outlander-0", modifier: 0, actions: []},
+        {limit: 2, text: "@outlander-1", modifier: 0, actions: []},
+        {limit: 3, text: "@outlander-2", modifier: 0, actions: []},
+        {limit: 4, text: "@outlander-3", modifier: 0, actions: []},
+        {limit: 5, text: "@outlander-4", modifier: 0, actions: []},
+        {limit: 6, text: "@outlander-5", modifier: 0, actions: []},
+    ]
+)
+
+export const sage = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@sage-0", modifier: 0, actions: []},
+        {limit: 2, text: "@sage-1", modifier: 0, actions: []},
+        {limit: 3, text: "@sage-2", modifier: 0, actions: []},
+        {limit: 4, text: "@sage-3", modifier: 0, actions: []},
+        {limit: 5, text: "@sage-4", modifier: 0, actions: []},
+        {limit: 6, text: "@sage-5", modifier: 0, actions: []},
+    ]
+)
+
+export const sailor = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@sailor-0", modifier: 0, actions: []},
+        {limit: 2, text: "@sailor-1", modifier: 0, actions: []},
+        {limit: 3, text: "@sailor-2", modifier: 0, actions: []},
+        {limit: 4, text: "@sailor-3", modifier: 0, actions: []},
+        {limit: 5, text: "@sailor-4", modifier: 0, actions: []},
+        {limit: 6, text: "@sailor-5", modifier: 0, actions: []},
+    ]
+)
+
+export const soldier = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@soldier-0", modifier: 0, actions: []},
+        {limit: 2, text: "@soldier-1", modifier: 0, actions: []},
+        {limit: 3, text: "@soldier-2", modifier: 0, actions: []},
+        {limit: 4, text: "@soldier-3", modifier: 0, actions: []},
+        {limit: 5, text: "@soldier-4", modifier: 0, actions: []},
+        {limit: 6, text: "@soldier-5", modifier: 0, actions: []},
+    ]
+)
+
+export const urchin = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@urchin-0", modifier: 0, actions: []},
+        {limit: 2, text: "@urchin-1", modifier: 0, actions: []},
+        {limit: 3, text: "@urchin-2", modifier: 0, actions: []},
+        {limit: 4, text: "@urchin-3", modifier: 0, actions: []},
+        {limit: 5, text: "@urchin-4", modifier: 0, actions: []},
+        {limit: 6, text: "@urchin-5", modifier: 0, actions: []},
+    ]
+)
+
+/* CLASS TRAINING */
+
+export const barbarian = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@barbarian-0", modifier: 0, actions: []},
+        {limit: 2, text: "@barbarian-1", modifier: 0, actions: []},
+        {limit: 3, text: "@barbarian-2", modifier: 0, actions: []},
+        {limit: 4, text: "@barbarian-3", modifier: 0, actions: []},
+        {limit: 5, text: "@barbarian-4", modifier: 0, actions: []},
+        {limit: 6, text: "@barbarian-5", modifier: 0, actions: []},
+    ]
+)
+
+export const bard = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@bard-0", modifier: 0, actions: []},
+        {limit: 2, text: "@bard-1", modifier: 0, actions: []},
+        {limit: 3, text: "@bard-2", modifier: 0, actions: []},
+        {limit: 4, text: "@bard-3", modifier: 0, actions: []},
+        {limit: 5, text: "@bard-4", modifier: 0, actions: []},
+        {limit: 6, text: "@bard-5", modifier: 0, actions: []},
+    ]
+)
+
+export const cleric = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@cleric-0", modifier: 0, actions: []},
+        {limit: 2, text: "@cleric-1", modifier: 0, actions: []},
+        {limit: 3, text: "@cleric-2", modifier: 0, actions: []},
+        {limit: 4, text: "@cleric-3", modifier: 0, actions: []},
+        {limit: 5, text: "@cleric-4", modifier: 0, actions: []},
+        {limit: 6, text: "@cleric-5", modifier: 0, actions: []},
+    ]
+)
+
+export const druid = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@druid-0", modifier: 0, actions: []},
+        {limit: 2, text: "@druid-1", modifier: 0, actions: []},
+        {limit: 3, text: "@druid-2", modifier: 0, actions: []},
+        {limit: 4, text: "@druid-3", modifier: 0, actions: []},
+        {limit: 5, text: "@druid-4", modifier: 0, actions: []},
+        {limit: 6, text: "@druid-5", modifier: 0, actions: []},
+    ]
+)
+
+export const fighter = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@fighter-0", modifier: 0, actions: []},
+        {limit: 2, text: "@fighter-1", modifier: 0, actions: []},
+        {limit: 3, text: "@fighter-2", modifier: 0, actions: []},
+        {limit: 4, text: "@fighter-3", modifier: 0, actions: []},
+        {limit: 5, text: "@fighter-4", modifier: 0, actions: []},
+        {limit: 6, text: "@fighter-5", modifier: 0, actions: []},
+    ]
+)
+
+export const monk = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@monk-0", modifier: 0, actions: []},
+        {limit: 2, text: "@monk-1", modifier: 0, actions: []},
+        {limit: 3, text: "@monk-2", modifier: 0, actions: []},
+        {limit: 4, text: "@monk-3", modifier: 0, actions: []},
+        {limit: 5, text: "@monk-4", modifier: 0, actions: []},
+        {limit: 6, text: "@monk-5", modifier: 0, actions: []},
+    ]
+)
+
+export const paladin = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@paladin-0", modifier: 0, actions: []},
+        {limit: 2, text: "@paladin-1", modifier: 0, actions: []},
+        {limit: 3, text: "@paladin-2", modifier: 0, actions: []},
+        {limit: 4, text: "@paladin-3", modifier: 0, actions: []},
+        {limit: 5, text: "@paladin-4", modifier: 0, actions: []},
+        {limit: 6, text: "@paladin-5", modifier: 0, actions: []},
+    ]
+)
+
+export const ranger = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@ranger-0", modifier: 0, actions: []},
+        {limit: 2, text: "@ranger-1", modifier: 0, actions: []},
+        {limit: 3, text: "@ranger-2", modifier: 0, actions: []},
+        {limit: 4, text: "@ranger-3", modifier: 0, actions: []},
+        {limit: 5, text: "@ranger-4", modifier: 0, actions: []},
+        {limit: 6, text: "@ranger-5", modifier: 0, actions: []},
+    ]
+)
+
+export const rogue = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@rogue-0", modifier: 0, actions: []},
+        {limit: 2, text: "@rogue-1", modifier: 0, actions: []},
+        {limit: 3, text: "@rogue-2", modifier: 0, actions: []},
+        {limit: 4, text: "@rogue-3", modifier: 0, actions: []},
+        {limit: 5, text: "@rogue-4", modifier: 0, actions: []},
+        {limit: 6, text: "@rogue-5", modifier: 0, actions: []},
+    ]
+)
+
+export const sorcerer = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@sorcerer-0", modifier: 0, actions: []},
+        {limit: 2, text: "@sorcerer-1", modifier: 0, actions: []},
+        {limit: 3, text: "@sorcerer-2", modifier: 0, actions: []},
+        {limit: 4, text: "@sorcerer-3", modifier: 0, actions: []},
+        {limit: 5, text: "@sorcerer-4", modifier: 0, actions: []},
+        {limit: 6, text: "@sorcerer-5", modifier: 0, actions: []},
+    ]
+)
+
+export const warlock = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@warlock-0", modifier: 0, actions: []},
+        {limit: 2, text: "@warlock-1", modifier: 0, actions: []},
+        {limit: 3, text: "@warlock-2", modifier: 0, actions: []},
+        {limit: 4, text: "@warlock-3", modifier: 0, actions: []},
+        {limit: 5, text: "@warlock-4", modifier: 0, actions: []},
+        {limit: 6, text: "@warlock-5", modifier: 0, actions: []},
+    ]
+)
+
+export const wizard = new Table(
+    new Dice(1, 6),
+    [
+        {limit: 1, text: "@wizard-0", modifier: 0, actions: []},
+        {limit: 2, text: "@wizard-1", modifier: 0, actions: []},
+        {limit: 3, text: "@wizard-2", modifier: 0, actions: []},
+        {limit: 4, text: "@wizard-3", modifier: 0, actions: []},
+        {limit: 5, text: "@wizard-4", modifier: 0, actions: []},
+        {limit: 6, text: "@wizard-5", modifier: 0, actions: []},
+    ]
+)
--- src/model/tables/index.ts
+++ src/model/tables/index.ts
@@ -1,8 +1,10 @@
 import * as origins from './origins'
+import * as decisions from './decisions'
 import * as supplemental from './supplemental'
 
 const tables = {
     origins: origins,
+    decisions: decisions,
     supplemental: supplemental,
 }
 
